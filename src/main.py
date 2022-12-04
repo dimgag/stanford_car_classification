5d965f4889a22ab028f5b10493cba5e589fb7432
dimgag <gagatsis94@gmail.com>
2022-12-04 11:24:05 +0100

    add lr scheduler StepLR

--- src/main.py
+++ src/main.py
@@ -18,16 +18,16 @@ from model import build_model, get_model_params
 from train import train, validate
 from utils import save_model, save_plots
 
-
+from torch.optim.lr_scheduler import StepLR
 
 if __name__ == "__main__":
     ## Dataset
     # Load the dataset directories
-    # train_dir = "car_data/car_data/train"
-    # valid_dir = "car_data/car_data/test"
+    train_dir = "car_data/car_data/train"
+    valid_dir = "car_data/car_data/test"
 
-    train_dir = "/Users/dim__gag/Desktop/stanford-cars-dataset/data/car_data/car_data/train"
-    valid_dir = "/Users/dim__gag/Desktop/stanford-cars-dataset/data/car_data/car_data/test"
+    # train_dir = "/Users/dim__gag/Desktop/stanford-cars-dataset/data/car_data/car_data/train"
+    # valid_dir = "/Users/dim__gag/Desktop/stanford-cars-dataset/data/car_data/car_data/test"
 
     # Show images from train directory
     train_images = os.listdir(train_dir)
@@ -50,32 +50,51 @@ if __name__ == "__main__":
     ## Get the fine tuned model
     model = build_model()
     # Show the model parameters
-    get_model_params(model)
+    # get_model_params(model)
 
 
-    # Define the device.
-    device = ('cuda' if torch.cuda.is_available() else 'cpu')
-    print(f"Computation device: {device}")
-    # Add the model to the device.
-    model = model.to(device)
+    # # Define the device.
+    # device = ('cuda' if torch.cuda.is_available() else 'cpu')
+    # print(f"Computation device: {device}")
+    # # Add the model to the device.
+    # model = model.to(device)
 
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     ## Compile the model with optimiser and loss function.
     # EXPERIMENTS WITH OTHER OPTIMISERS AND OTHER LEARNING RATES CAN BE DONE HERE
-    # optimizer = optim.Adam(model.parameters(), lr=0.0001)
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    
+    optimizer = optim.Adam(model.parameters(), lr=0.1)
+    # optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+    
+    ## Adam optimiser + Step Lr Scheduler
+    
+    scheduler = StepLR(optimizer, step_size=5, gamma=0.1)
+            
+            
     # Loss function.
     criterion = nn.CrossEntropyLoss()
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-
+    
     ## Train the model
     # Lists to keep track of the loss and accuracy.
     train_loss, valid_loss = [], []
     train_acc, valid_acc = [], []
-
+    
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+    
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+    
+    
     for epoch in range(epochs):
+        # Decay Learning Rate
+        scheduler.step()
+        # Print Learning Rate
+        print('LR:', scheduler.get_lr())
+        # Print Num Epoch
         print(f"Epoch {epoch+1} of {epochs}")
         train_epoch_loss, train_epoch_acc = train(model, train_loader, 
                                                 optimizer, criterion)
