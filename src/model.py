730064ca78c32699749ab867dc00958d33176413
dimgag <gagatsis94@gmail.com>
2022-12-11 19:29:20 +0100

    commit changes

--- src/model.py
+++ src/model.py
@@ -43,23 +43,21 @@ def build_model(pretrained=True, freeze=False, num_classes=196):
   
   print("\nAdding Classification Head . . .")
   # Add the classification head
-  
   # Classficication Head 1
-  # model.classifier[1] = nn.Linear(in_features=1280, out_features=num_classes).to(device)
+  model.classifier[1] = nn.Linear(in_features=1280, out_features=num_classes).to(device)
   
 
   # Classficication Head 2
-  #     Results 2 - good results
-  # model.classifier = torch.nn.Sequential(
-  #   torch.nn.Linear(in_features=1280, out_features=640, bias=True),
-  #   torch.nn.Dropout(p=0.2, inplace=True),
-  #   torch.nn.Linear(in_features=640, out_features=320, bias=True),
-  #   torch.nn.Dropout(p=0.2, inplace=True),
-  #   torch.nn.Linear(in_features=320, out_features=num_classes, bias=True)).to(device)
+  model.classifier = torch.nn.Sequential(
+    torch.nn.Linear(in_features=1280, out_features=640, bias=True),
+    torch.nn.Dropout(p=0.2, inplace=True),
+    torch.nn.Linear(in_features=640, out_features=320, bias=True),
+    torch.nn.Dropout(p=0.2, inplace=True),
+    torch.nn.Linear(in_features=320, out_features=num_classes, bias=True)).to(device)
 
 
 
-  # Classficication Head 3 maybe this one 
+  # Classficication Head 3
   model.classifier = nn.Sequential(
     nn.Linear(in_features=1280, out_features=640),
     nn.Linear(in_features=640, out_features=320),
@@ -74,20 +72,4 @@ def build_model(pretrained=True, freeze=False, num_classes=196):
   get_model_params(model)
   print("-"*50)
 
-  return model    
-
-
-'''
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-from torchinfo import summary
-
-model = build_model()
-
-summary(model, 
-        input_size=(32, 3, 224, 224), # make sure this is "input_size", not "input_shape" (batch_size, color_channels, height, width)
-        verbose=0,
-        col_names=["input_size", "output_size", "num_params", "trainable"],
-        col_width=20,
-        row_settings=["var_names"]
-)'''
-
+  return model
